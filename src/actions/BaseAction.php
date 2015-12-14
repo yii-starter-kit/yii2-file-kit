e452844861b0092ad7e10c5dd968abe3d5e1aad8
Eugene Terentev <eugene@terentev.net>
2015-12-14 14:25:48 +0200

    `allowChangeFilestorage` added

--- src/actions/BaseAction.php
+++ src/actions/BaseAction.php
@@ -1,7 +1,9 @@
 <?php
 namespace trntv\filekit\actions;
 
+use trntv\filekit\Storage;
 use yii\base\Action;
+use yii\di\Instance;
 
 /**
  * Class BaseAction
@@ -22,19 +24,24 @@ abstract class BaseAction extends Action
      * @var string session key to store list of uploaded files
      */
     public $sessionKey = '_uploadedFiles';
+    /**
+     * Allows users to change filestorage by passing GET variable
+     * @var bool
+     */
+    public $allowChangeFilestorage = false;
 
     /**
      * @return \trntv\filekit\Storage
-     * @throws \HttpException
      * @throws \yii\base\InvalidConfigException
      */
     protected function getFileStorage()
     {
-        $fileStorageId = \Yii::$app->request->get($this->fileStorageParam, $this->fileStorage);
-        $fileStorage = \Yii::$app->get($fileStorageId);
-        if (!$fileStorage) {
-            throw new \HttpException(400);
+        if ($this->allowChangeFilestorage) {
+            $fileStorage = \Yii::$app->request->get($this->fileStorageParam);
+        } else {
+            $fileStorage = $this->fileStorage;
         }
+        $fileStorage = Instance::ensure($fileStorage, Storage::className());
         return $fileStorage;
     }
 }
