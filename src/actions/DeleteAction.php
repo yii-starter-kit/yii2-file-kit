c64a8f988bcda07ac8f4764a590dce358759a23d
notemood <notemood@users.noreply.github.com>
2016-06-26 18:56:46 +0300

    Added EVENT_AFTER_DELETE
    Added an event to run after delete action. 
    Scenario:
    If you create additional images (eg thumbnails) on afterSave() event now you can delete them using afterDelete() event in your action

--- src/actions/DeleteAction.php
+++ src/actions/DeleteAction.php
@@ -9,18 +9,27 @@
 namespace trntv\filekit\actions;
 
 use yii\web\HttpException;
+use League\Flysystem\FilesystemInterface;
+use trntv\filekit\events\UploadEvent;
+use League\Flysystem\File as FlysystemFile;
 
 /**
  * public function actions(){
  *   return [
  *           'upload'=>[
  *               'class'=>'trntv\filekit\actions\DeleteAction',
+ *               'on afterDelete' => function($event) {
+ *                   $file = $event->file;
+ *                   $thumb_path = Yii::getAlias('@storage/web/source/thumb/') . $file->getPath();
+ *                   unlink($thumb_path);
+ *              }
  *           ]
  *       ];
  *   }
  */
 class DeleteAction extends BaseAction
 {
+    const EVENT_AFTER_DELETE = 'afterDelete';
     /**
      * @var string path request param
      */
@@ -38,10 +47,29 @@ class DeleteAction extends BaseAction
             $success = $this->getFileStorage()->delete($path);
             if (!$success) {
                 throw new HttpException(400);
+            } else {
+                $this->afterDelete($path);
             }
             return $success;
         } else {
             throw new HttpException(403);
         }
+    }
+    
+    /**
+     * @param $path
+     */
+    public function afterDelete($path)
+    {
+        $file = null;
+        $fs = $this->getFileStorage()->getFilesystem();
+        if ($fs instanceof FilesystemInterface) {
+            $file = new FlysystemFile($fs, $path);
+        }
+        $this->trigger(self::EVENT_AFTER_DELETE, new UploadEvent([
+            'path' => $path,
+            'filesystem' => $fs,
+            'file' => $file
+        ]));
     }
 }
