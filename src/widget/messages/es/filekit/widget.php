bfa4ace9983851e7ec4240cc09a63d730e2b34a9
xavier Villamuera <gzav@fifteenpeas.com>
2017-03-15 16:52:26 +0100

    added translation : fr and es

---
+++ src/widget/messages/es/filekit/widget.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Project yii2-file-kit.
+ * Author: Eugene Terentev <eugene@terentev.net>
+ */
+return [
+    'Maximum number of files exceeded' => 'Numero maximo de ficheros acceptado ',
+    'File type not allowed' => 'Typos de ficheros acceptados',
+    'File is too large' => 'Fichero demasiado grande',
+    'File is too small' => 'Fichero demasiado pequeño'
+];
