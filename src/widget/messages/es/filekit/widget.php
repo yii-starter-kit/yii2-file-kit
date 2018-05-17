490d0ae31d507ac101f1aaa39d3f41b573de70e9
Victor Gonzalez <victor.gonzalezro@gmail.com>
2018-05-17 17:27:26 -0400

    fix spanish translation
    Signed-off-by: Victor Gonzalez <victor.gonzalezro@gmail.com>

--- src/widget/messages/es/filekit/widget.php
+++ src/widget/messages/es/filekit/widget.php
@@ -4,8 +4,8 @@
  * Author: Eugene Terentev <eugene@terentev.net>
  */
 return [
-    'Maximum number of files exceeded' => 'Numero maximo de ficheros acceptado ',
-    'File type not allowed' => 'Typos de ficheros acceptados',
+    'Maximum number of files exceeded' => 'Se ha superado el límite de archivos permitidos',
+    'File type not allowed' => 'Formato de archivo no permitido',
     'File is too large' => 'Fichero demasiado grande',
     'File is too small' => 'Fichero demasiado pequeño'
 ];
